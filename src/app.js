07064b0a62563053b25bce2708d969f0e9cc5224
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-02-27 17:35:03 +0900

    Feat: add package

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+
+require('dotenv').config();
+
+// const connectDatabase = require('./db');
+// connectDatabase();
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(cors({ credentials: true }));
+app.use(express.json({ limit: '12mb' }));
+app.use(express.urlencoded({ extended: true, limit: '12mb' }));
+
+// const initApi = require('./api');
+// initApi(app);
+
+app.listen(process.env.PORT_NUM, () => {
+  console.log(`Server start from ${process.env.PORT_NUM} port`);
+});
